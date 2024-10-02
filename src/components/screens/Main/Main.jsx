ce38a92e947fc94fc90a7f3b37d94b51b4644c90
iskillhiro <155888748+iskillhiro@users.noreply.github.com>
2024-10-02 22:02:34 +0300

    add balance

--- src/components/screens/Main/Main.jsx
+++ src/components/screens/Main/Main.jsx
@@ -2,11 +2,22 @@ import React, { useEffect, useState } from 'react'
 import { axiosDB } from '../../../utils/axiosDB'
 import Holders from '../../items/Holders/Holders.jsx'
 import './Main.css'
+
 export default function Main() {
 	const [allHolders, setAllHolders] = useState([])
 	const [loading, setLoading] = useState(true)
+	const [timeElapsed, setTimeElapsed] = useState(0) // Timer state
 
 	useEffect(() => {
+		let timer
+
+		if (loading) {
+			// Start the timer when loading starts
+			timer = setInterval(() => {
+				setTimeElapsed(prevTime => prevTime + 1)
+			}, 1000)
+		}
+
 		const fetchData = async () => {
 			try {
 				const response = await axiosDB.get()
@@ -14,14 +25,36 @@ export default function Main() {
 				setLoading(false)
 			} catch (error) {
 				console.error('Error fetching holders:', error)
+			} finally {
+				clearInterval(timer) // Stop the timer when loading is complete
 			}
 		}
 
 		fetchData()
-	}, [])
+
+		// Cleanup the timer when the component is unmounted
+		return () => {
+			clearInterval(timer)
+		}
+	}, [loading])
 
 	if (loading) {
-		return <div className='loader'></div>
+		return (
+			<>
+				<div className='loader'></div>
+				<p
+					style={{
+						color: 'white',
+						padding: '40px',
+						width: 'max-content',
+						textWrap: 'nowrap',
+						margin: 'auto',
+					}}
+				>
+					{timeElapsed} seconds
+				</p>
+			</>
+		)
 	}
 
 	return (
